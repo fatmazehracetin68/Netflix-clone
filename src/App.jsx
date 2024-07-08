7cafbcadbb8d9b41a791058ff159bb8586ec5ed4
fatmazehracetin68 <fatimatuzehra.cetin@gmail.com>
2024-07-08 10:41:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+function App() {
+  return (
+    <Router>
+      <div className="p-5 md:p-10 lg:p-16 xl:p-20">
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/movies/:id" element={<DetailPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+export default App;
